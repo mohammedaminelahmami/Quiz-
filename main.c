5c6dc7146b1cb956b4fa443ee90a90e13d177174
mohammedaminelahmami <lahmamimohammedamine@gmail.com>
2021-11-11 10:22:03 +0000

    version 1

---
+++ main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	int a;
+	int p = 0;
+	int i;
+
+	printf("|| Bienvenue au Quiz ! ||\n");
+	
+		
+	for(i = 1; i <= 4; i++){
+	
+	switch(i){
+		case 1: {
+			printf("\t\t\t\tQ1 : 500 + 23 = ?? \n 1- 552\n 2- 540\n 3- 523\n");
+			scanf("%d", &a);
+			if(a == 3){
+				printf("Correct Answer !! + %d Pnts", p+=2);
+			}else{
+				printf("Wrong Answer !! - %dPnts", p-=1);
+			}
+			break;
+		}
+		case 2: {
+			printf("\tQ1 : 17 + 23 = ?? \n 1- 30\n 2- 40\n 3- 25\n");
+			scanf("%d", &a);
+			if(a == 2){
+				printf("Correct Answer !!  %d Pnts", p+=2);
+			}else{
+				printf("Wrong Answer !! - %dPnts", p-=1);
+			}
+			break;
+		}
+		case 3: {
+			printf("\tQ1 : 11 + 23 = ?? \n 1- 34\n 2- 40\n 3- 25\n");
+			scanf("%d", &a);
+			if(a == 1){
+				printf("Correct Answer !! %d Pnts", p+=2);
+			}else{
+				printf("Wrong Answer !!  %dPnts", p-=1);
+			}
+			break;
+		}
+	}
+	}
+	
+	printf("\tResult : %d Pnts !!", p);
+	
+	
+
+
+ 
+ 
+
+
+
+
+
+
+
+	
+
+	return 0;
+}
